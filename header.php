27ccb485dd6077f7dd354560aa8e88c58df617e9
AdnanSB <Adnansb@outlook.dk>
2020-11-25 13:30:37 +0100

    menu changes

--- header.php
+++ header.php
@@ -15,7 +15,27 @@
         <img id="logo" src="<?php echo get_template_directory_uri(); ?>/assets/heartflow.svg" alt="Logo" width="334.29" height="98.91" />
     </a>
 
+ 
+ 
 
+  <div id="menuToggle">
+      <input type="checkbox" />
+      <div id="burgermenu">Menu</div>
+      <ul id="menu">
+      <a href="<?php echo esc_url(home_url('/')); ?>" ><li>Forside</li></a>    
+      <hr>   
+      <a href="<?php echo esc_url(home_url('/begivenheder'));?>"><li>Begivenheder</li></a>
+      <hr>
+      <a href="<?php echo esc_url(home_url('/om-os')); ?>"><li>Om Os</li></a>  
+      <hr>
+      <a href="<?php echo esc_url(home_url('/donation'));?>"><li>Fri Donation</li></a> 
+      <hr>           
+      <a href="<?php echo esc_url(home_url('/kontakt'));?>"><li>Kontakt</li></a>       
+
+      </ul>
+    </div>
+
+    
     <?php
     wp_nav_menu(array("theme_location" => "header-menu"))
     ?>
